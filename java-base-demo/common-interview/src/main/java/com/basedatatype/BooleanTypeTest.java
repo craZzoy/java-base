a4bfdce392be09ceef8e344e8d9b9fc97deeff8b
zhengwz <1510134048@qq.com>
2021-07-26 17:19:56 +0800

    add notes

---
+++ java-base-demo/common-interview/src/main/java/com/basedatatype/BooleanTypeTest.java
@@ -0,0 +1,15 @@
+package com.basedatatype;
+
+/**
+ * @Description:
+ * @Author : 郑玮泽
+ * @Date : 15:38 2021/7/23
+ */
+public class BooleanTypeTest {
+
+    public static void main(String[] args) {
+        boolean b = true;
+        System.out.println(b);
+    }
+
+}
---
+++ java-base-demo/virtual-machine-demo/src/main/java/com/test/compier/AutoBoxTrap.java
@@ -0,0 +1,22 @@
+package com.test.compier;
+
+/**
+ * 自动拆箱、装箱陷阱
+ * @Description:
+ * @Author : 郑玮泽
+ * @Date : 17:17 2021/7/26
+ */
+public class AutoBoxTrap {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 3;
+        int e = 321;
+        int f = 321;
+        Long g = 3L;
+        System.out.println();
+    }
+
+}
